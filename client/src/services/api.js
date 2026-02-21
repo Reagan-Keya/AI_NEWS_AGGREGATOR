6c7f4a54ad0a23b471829c8ac572babc1575bd6c
Reagan-Keya <owitikeya1@gmail.com>
2026-02-21 14:51:03 +0300

    Change BASE_URL to use environment variable

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:5000/api/news";
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 export const fetchNews = async (category = "") => {
   try {
